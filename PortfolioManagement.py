c35a108a4503779d19fe0cdacb063fc1964aac1c
Unknown <odysseasyiakoumis@gmail.com>
2019-04-05 18:24:15 +0200

    LSTM MultiLayer w/ DropOut

--- PortfolioManagement.py
+++ PortfolioManagement.py
@@ -3,7 +3,7 @@ import keras
 
 class PortfolioManagement:
 
-    def __init__(self, batch_size, total_time, n_stocks, transaction_costs, alpha_parameter, learning_rate, n_neurons):
+    def __init__(self, batch_size, total_time, n_stocks, transaction_costs, alpha_parameter, learning_rate, n_neurons, n_layers):
 
         self.batch_size = batch_size
         self.total_time = total_time
@@ -12,6 +12,8 @@ class PortfolioManagement:
         self.alpha_parameter = alpha_parameter
         self.learning_rate = learning_rate
         self.n_neurons = n_neurons
+        self.n_layers = n_layers
+        self.keep_prob = tf.placeholder_with_default(1.0, shape=())
 
         self.n_inputs = n_stocks
         self.n_outputs = n_stocks
@@ -20,13 +22,16 @@ class PortfolioManagement:
         self.X = tf.placeholder(dtype=tf.float32, shape=[None, self.total_time, self.n_inputs], name="Inputs")
         self.y = tf.placeholder(dtype=tf.float32, shape=[None, self.total_time, self.n_outputs], name="Targets")
 
-        self.cell = tf.nn.rnn_cell.BasicRNNCell(num_units=self.n_neurons, activation=tf.nn.relu)
-        self.cell_wrapped = tf.contrib.rnn.OutputProjectionWrapper(self.cell, self.n_outputs, activation = tf.nn.softmax)
+        self.cell = tf.contrib.rnn.BasicLSTMCell
+        self.cell_drop = tf.contrib.rnn.DropoutWrapper
+        self.multi_cell = tf.contrib.rnn.MultiRNNCell([self.cell_drop(self.cell(num_units=self.n_neurons,
+                            activation=tf.nn.relu), input_keep_prob=self.keep_prob) for _ in range(self.n_layers)])
+        self.cell_wrapped = tf.contrib.rnn.OutputProjectionWrapper(self.multi_cell, self.n_outputs, activation = tf.nn.softmax)
         self.outputs, self.states = tf.nn.dynamic_rnn(self.cell_wrapped, self.X, dtype=tf.float32)
 
         self.ptf_mean, self.ptf_var = tf.nn.moments(self.outputs * self.y, axes=2) # element-wise multiplication
         self.loss = tf.reduce_mean(tf.reduce_mean(self.ptf_var - self.alpha_parameter * self.ptf_mean, axis=1) + \
-                    self.transaction_costs * tf.reduce_sum(self.states,axis=1), axis=0)
+                    self.transaction_costs * tf.reduce_sum(self.outputs[:,-1,:],axis=1), axis=0)
 
         self.optimizer = tf.train.AdamOptimizer(self.learning_rate)
         self.training_op = self.optimizer.minimize(self.loss)
--- main.py
+++ main.py
@@ -10,8 +10,9 @@ n_stocks = 5
 transaction_costs = 0.2
 alpha_parameter = 1.5
 learning_rate = 0.001
-n_neurons = 100
+n_neurons = 10
 n_layers = 3
+keep_prob = 0.5
 
 # simulation parameters:
 total_time = 30
@@ -22,17 +23,18 @@ mu = np.full((n_stocks,1),0.01)
 sigma = np.full((n_stocks,1),0.1)
 
 portefeuille = PortfolioManagement(batch_size, total_time, n_stocks, transaction_costs,
-                                      alpha_parameter, learning_rate, n_neurons)
+                                      alpha_parameter, learning_rate, n_neurons, n_layers)
 
 portefeuille.build()
 
-n_iterations = 10000
+n_iterations = 1000
 batch_size = portefeuille.batch_size
 
 with tf.Session() as sess:
     sess.run(portefeuille.init)
     for iteration in range(n_iterations):
         X_batch, y_batch = sim.next_batch(batch_size, n_stocks,n_steps,total_time,inital_values,mu,sigma)
-        _, loss_eval = sess.run([portefeuille.training_op, portefeuille.loss], feed_dict={portefeuille.X: X_batch, portefeuille.y: y_batch})
+        _, loss_eval = sess.run([portefeuille.training_op, portefeuille.loss], feed_dict={portefeuille.X: X_batch,
+                                                            portefeuille.y: y_batch, portefeuille.keep_prob: keep_prob})
         if iteration % 100 == 0:
             print(iteration, "\tloss:", loss_eval)
